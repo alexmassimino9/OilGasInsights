7fe57c58412f36f408cec57a9d9506290a9043f6
Alexander Massimino <49351994+alexmassimino9@users.noreply.github.com>
2024-03-21 04:39:48 -0400

    linting, minor update

--- src/components/TubingPressureGraph.tsx
+++ src/components/TubingPressureGraph.tsx
@@ -9,11 +9,11 @@ import {
   ResponsiveContainer,
   Brush,
 } from "recharts";
-import { CustomTooltip } from "../views/tubingPressure"; // Adjust import path as necessary
+import { CustomTooltip } from "../views/tubingPressure"; // Ensure this is the correct path
 import { Paper, Typography, Box, useTheme } from "@mui/material";
-import Config from "../config"; // Adjust import path as necessary
+import Config from "../config"; // Ensure this is the correct path
+import { tokens } from "../theme";
 
-// Define interfaces for the expected structure of Config.data
 interface ConfigStructure {
   headers: string[];
   units_of_measure: string[];
@@ -24,50 +24,78 @@ interface ChartData {
   timestamp: string;
   tubingPressure: number;
 }
+const aggregateData = (data: ChartData[], maxPoints: number): ChartData[] => {
+  const interval = Math.ceil(data.length / maxPoints);
+  return data.reduce<ChartData[]>((acc, curr, index) => {
+    if (index % interval === 0) {
+      const end =
+        index + interval < data.length ? index + interval : data.length;
+      let sum = 0;
+      for (let i = index; i < end; i++) {
+        sum += data[i].tubingPressure;
+      }
+      const avg = sum / (end - index);
+      acc.push({ timestamp: curr.timestamp, tubingPressure: avg });
+    }
+    return acc;
+  }, []);
+};
 
 const TubingPressureGraph: React.FC = () => {
   const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
   const data: ChartData[] = useMemo(() => {
     const configData: ConfigStructure = Config.data as ConfigStructure;
-
     const timestampIndex = configData.headers.indexOf("timestamp");
     const tubingPressureIndex = configData.headers.indexOf("tubing_pressure");
 
-    return configData.data[timestampIndex].map((time, index) => ({
-      timestamp: new Date(time * 1000).toLocaleString("en-US", {
-        hour12: false,
+    const rawData: ChartData[] = configData.data[timestampIndex].map(
+      (time, index) => ({
+        timestamp: new Date(time * 1000).toLocaleString("en-US", {
+          hour12: false,
+        }),
+        tubingPressure: configData.data[tubingPressureIndex][index],
       }),
-      tubingPressure: configData.data[tubingPressureIndex][index],
-    }));
+    );
+
+    return aggregateData(rawData, 500);
   }, []);
 
   return (
-    <Paper sx={{ p: 2, m: 1, overflow: "hidden" }} elevation={3}>
+    <Paper
+      sx={{
+        p: 2,
+        m: 1,
+        overflow: "hidden",
+        backgroundColor: theme.palette.background.default,
+      }}
+      elevation={3}
+    >
       <Typography
         variant="h6"
         component="h2"
         gutterBottom
-        sx={{ color: theme.palette.text.primary }}
+        sx={{ color: colors.green[500] }}
       >
         Tubing Pressure Over Time
       </Typography>
       <Box sx={{ width: "100%", height: "300px" }}>
         <ResponsiveContainer width="100%" height="100%">
           <LineChart data={data}>
-            <defs>
-              <linearGradient id="colorPressure" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-              </linearGradient>
-            </defs>
-            <Brush dataKey="timestamp" height={30} stroke="#8884d8" />
+            {/* Gradient definition remains the same */}
+            <Brush
+              dataKey="timestamp"
+              height={30}
+              stroke={colors.mirage[300]}
+            />
             <XAxis
               dataKey="timestamp"
-              tick={{ fill: theme.palette.text.secondary, fontSize: 12 }}
+              tick={{ fill: colors.grey[900], fontSize: 12 }}
               tickFormatter={(timestamp) =>
                 new Date(timestamp).toLocaleTimeString("en-US", {
-                  hour12: true,
-                  timeStyle: "short",
+                  hour12: false,
+                  hour: "2-digit",
+                  minute: "2-digit",
                 })
               }
               angle={-45}
@@ -79,23 +107,28 @@ const TubingPressureGraph: React.FC = () => {
                 value: "Pressure (psi)",
                 angle: -90,
                 position: "insideLeft",
-                fill: theme.palette.text.secondary,
+                fill: colors.green[500],
+                fontSize: "1rem",
               }}
             />
             <CartesianGrid
-              strokeDasharray="3 3"
-              stroke={theme.palette.divider}
+              // strokeDasharray="9 9"
+              stroke={"none"}
             />
             <Tooltip content={<CustomTooltip />} />
             <Line
               type="monotone"
               dataKey="tubingPressure"
-              stroke="#8884d8"
-              strokeWidth={2}
-              dot={false}
-              activeDot={{ r: 8 }}
+              stroke={theme.palette.secondary.main}
+              strokeWidth={3}
+              dot={{
+                stroke: theme.palette.secondary.dark,
+                strokeWidth: 2,
+                r: 4,
+              }}
+              activeDot={{ r: 6 }}
               fillOpacity={1}
-              fill="url(#colorPressure)"
+              fill="url(#pressureGradient)"
             />
           </LineChart>
         </ResponsiveContainer>
--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,37 +1,38 @@
-import React, {useState,useContext,createContext,ReactNode} from "react";
-import useMediaQuery from '@mui/material/useMediaQuery';
+import React, { useState, useContext, createContext, ReactNode } from "react";
+import useMediaQuery from "@mui/material/useMediaQuery";
 interface AppContextType {
-    open: boolean;
-    desktop: boolean;
-    handleDrawerClose: () => void;
-    handleDrawerOpen: () => void;
+  open: boolean;
+  desktop: boolean;
+  handleDrawerClose: () => void;
+  handleDrawerOpen: () => void;
 }
 
 interface AppProviderProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 const AppContext = createContext<AppContextType>({
-
-open: true, 
-desktop: false, 
-handleDrawerClose: () => {}, 
-handleDrawerOpen: () => {},
+  open: true,
+  desktop: false,
+  handleDrawerClose: () => {},
+  handleDrawerOpen: () => {},
 });
 
 const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
-    const [open,setOpen] = useState(false);
-    const desktop = useMediaQuery("(min-width:600px)");
-    const handleDrawerClose = () => setOpen(false);
-    const handleDrawerOpen = () => setOpen(true);
+  const [open, setOpen] = useState(false);
+  const desktop = useMediaQuery("(min-width:600px)");
+  const handleDrawerClose = () => setOpen(false);
+  const handleDrawerOpen = () => setOpen(true);
 
-    return (
-        <AppContext.Provider value={{open, desktop, handleDrawerClose, handleDrawerOpen}}>
-            {children}
-        </AppContext.Provider>
-    );
-}
+  return (
+    <AppContext.Provider
+      value={{ open, desktop, handleDrawerClose, handleDrawerOpen }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
 export const useGlobalContext = () => {
-    return useContext(AppContext);
-  };
-  
+  return useContext(AppContext);
+};
+
 export { AppContext, AppProvider };
