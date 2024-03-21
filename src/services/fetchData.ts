a9b02cff5d2a68a7c8010f2801b209a4d64c1505
Alexander Massimino <49351994+alexmassimino9@users.noreply.github.com>
2024-03-21 18:48:26 -0400

    prod api

--- src/services/fetchData.ts
+++ src/services/fetchData.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from "axios";
 import { ApiData } from "../types/dataModels";
 import Config from "../config";
 export const fetchData = async (): Promise<ApiData> => {
-  const response: AxiosResponse<ApiData> = await axios.get(Config.mockURL);
+  const response: AxiosResponse<ApiData> = await axios.get(Config.API_URL);
   console.log("data", response.data);
   return response.data;
 };
