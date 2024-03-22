5a174b2b519df680de1c659135e883b7070e9b17
Alexander Massimino <49351994+alexmassimino9@users.noreply.github.com>
2024-03-22 03:46:54 -0400

    init mvp

--- README.md
+++ README.md
@@ -1,30 +1,64 @@
-# React + TypeScript + Vite
+# Pressure Data Visualization Tool for Demo Company Oil and Gas
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This project is a web-based application designed to visualize time series data related to pressure readings across different depths for Demo Company Oil and Gas. It aims to users with an intuitive and interactive tool to understand complex data through visual representations.
 
-Currently, two official plugins are available:
+## Objective
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+The primary goal is to develop an interactive web application that fetches time series data from a provided API and visualizes this data in a user-friendly manner. The focus is on tubing and casing pressures over depth, offering insights into pressure variations and anomalies in an accessible format.
 
-## Expanding the ESLint configuration
+## Key Features
 
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
+- **Data Fetching**: Efficiently retrieves time series data from an external API, handling asynchronous operations with React Query for optimal performance and user experience.
+- **Interactive Charts**: Utilizes Recharts to render responsive and interactive line charts, displaying tubing and casing pressures over depth.
+- **Responsive Design**: Crafted with Material-UI, the application is fully responsive, ensuring usability across various devices and screen sizes.
+- **Customizable Themes**: Incorporates Material-UI's theming capabilities to offer a consistent look and feel that aligns with Demo Company Oil and Gas's branding requirements.
+- **Accessibility**: Designed with accessibility in mind, ensuring that the tool is usable by as wide an audience as possible.
 
-- Configure the top-level `parserOptions` property like this:
+## Technology Stack
 
-```js
-export default {
-  // other rules...
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  },
-}
-```
+- **Vite**: Used for setting up the React project, offering fast builds and an efficient development experience.
+- **React**: Powers the user interface, adopting a component-based architecture for scalable and maintainable code.
+- **TypeScript**: Enhances code quality and developer experience with static type checking.
+- **Recharts**: Chosen for crafting interactive and responsive data visualizations.
+- **Material-UI**: Provides a comprehensive suite of UI components and styling solutions.
+- **React Query**: Manages asynchronous data fetching, caching, and synchronization.
 
-- Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
-- Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+## Getting Started
+
+To get a local copy up and running, follow these simple steps.
+
+### Prerequisites
+
+- npm
+```sh
+npm install npm@latest -g
+Installation
+Clone the repository
+sh
+Copy code
+git clone https://github.com/alexmassimino9/oil-gas-insights
+Install NPM packages
+sh
+Copy code
+npm install
+Start the development server
+sh
+Copy code
+npm run dev
+
+License
+Distributed under the MIT License. See LICENSE for more information.
+
+Contact
+Your Name - alexmassimino9@gmail.com
+
+Project Link: https://oil-gas-insights.vercel.app/
+
+Acknowledgements
+React
+TypeScript
+Recharts
+Material-UI
+React Query
+Vite
+DWS
--- src/views/topbar/Topbar.tsx
+++ src/views/topbar/Topbar.tsx
@@ -1,8 +1,19 @@
-import { useContext } from "react";
-import { Box, useTheme, IconButton } from "@mui/material";
+import { useContext, useState } from "react";
+import {
+  Box,
+  useTheme,
+  Modal,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  Link,
+} from "@mui/material";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
-import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import WebIcon from "@mui/icons-material/Web";
+import GitHubIcon from "@mui/icons-material/GitHub";
 import { ColorModeContext, tokens } from "../../theme";
 import { useGlobalContext } from "../../context/AppContext";
 import Config from "../../config";
@@ -12,6 +23,16 @@ const Topbar = () => {
   const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
   const { desktop } = useGlobalContext();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <Box
       display="flex"
@@ -41,23 +62,91 @@ const Topbar = () => {
       <Box
         display="flex"
         sx={{
-          "& :hover": { backgroundColor: "inherit", color: colors.grey[100] },
+          justifyContent: "space-between",
+          alignItems: "center",
+          "& :hover": {
+            backgroundColor: "inherit",
+            color: colors.grey[100],
+          },
         }}
       >
-        <IconButton
-          onClick={colorMode.toggleColorMode}
-          aria-label="Toggle theme"
-        >
-          {theme.palette.mode === "dark" ? (
-            <DarkModeOutlinedIcon />
-          ) : (
-            <LightModeOutlinedIcon />
-          )}
-        </IconButton>
-        <IconButton aria-label="Settings">
-          <SettingsOutlinedIcon />
-        </IconButton>
+        {theme.palette.mode === "dark" ? (
+          <DarkModeOutlinedIcon
+            onClick={colorMode.toggleColorMode}
+            aria-label="Toggle theme"
+            sx={{ marginInline: "1rem" }}
+          />
+        ) : (
+          <LightModeOutlinedIcon
+            onClick={colorMode.toggleColorMode}
+            aria-label="Toggle theme"
+            sx={{ marginInline: "1rem" }}
+          />
+        )}
+
+        <InfoOutlinedIcon
+          aria-label="Personal Links"
+          onClick={handleOpenModal}
+          sx={{ marginInline: "1rem" }}
+        />
       </Box>
+
+      {/* Personal links modal */}
+      <Modal open={isModalOpen} onClose={handleCloseModal}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            bgcolor: colors.tasman[200],
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <Typography variant="h5" sx={{ mb: 2, color: colors.grey[900] }}>
+            Personal Links
+          </Typography>
+          <List>
+            <ListItem>
+              <ListItemText
+                primary={
+                  <Link
+                    href="https://alexmassimino.com"
+                    sx={{ color: colors.grey[900] }}
+                  >
+                    <WebIcon /> Personal Website
+                  </Link>
+                }
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                primary={
+                  <Link
+                    href="https://github.com/alexmassimino9"
+                    sx={{ mb: 2, color: colors.grey[900] }}
+                  >
+                    <GitHubIcon /> GitHub Profile
+                  </Link>
+                }
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                primary={
+                  <Link
+                    href="https://github.com/alexmassimino9/oil-gas-insights"
+                    sx={{ mb: 2, color: colors.grey[900] }}
+                  >
+                    <GitHubIcon /> Source Code
+                  </Link>
+                }
+              />
+            </ListItem>
+          </List>
+        </Box>
+      </Modal>
     </Box>
   );
 };
